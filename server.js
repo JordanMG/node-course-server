24edd205c3fe569de39bb16c251e2f0fd3524fb9
JordanMG <jordangreer@gmail.com>
2018-03-08 19:06:19 -0500

    Attempting to fix heroku port problem

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
-const port = process.env.port || 3000   // for Heroku, either listen to the environment port or use 3000 by default
+const port = process.env.PORT || 3000   // for Heroku, either listen to the environment port or use 3000 by default
 var app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
